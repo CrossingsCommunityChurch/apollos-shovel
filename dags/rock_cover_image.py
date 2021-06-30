0481f5b25f2cf84a670d8b67779229f320d245ac
Tom Thornton <tom@differential.com>
2021-06-30 11:17:12 -0400

    Update rock_cover_image.py

--- dags/rock_cover_image.py
+++ dags/rock_cover_image.py
@@ -51,15 +51,24 @@ def fetch_and_save_cover_image(ds, *args, **kwargs):
     skip = 0
     top = 10000
 
+    def update_content_item_cover_image(args):
+        contentItemId = args['ContentItemId']
+        coverImageId = args['CoverImageId']
+        return pg_hook.run(
+            'UPDATE "contentItems" SET "coverImageId" = (SELECT id FROM "media" WHERE "originId" = %s) WHERE "originId"::Integer = %s', 
+            True, 
+            (coverImageId, contentItemId))
+
     def map_content_items(content_item):
 
         imageAttributes = get_images(content_item)
         coverImageId = get_best_image_id(imageAttributes, pg_hook)
 
         if(coverImageId):
-            return {
+            update_content_item_cover_image({
                 "ContentItemId": content_item['Id'],
                 "CoverImageId": coverImageId
+<<<<<<< Updated upstream
             }
         else:
             contentItemId = content_item['Id']
@@ -89,6 +98,12 @@ def fetch_and_save_cover_image(ds, *args, **kwargs):
             contentItemId = args['ContentItemId']
             coverImageId = args['CoverImageId']
             return pg_hook.run('UPDATE "contentItems" SET "coverImageId" = %s WHERE "originId"::Integer = %s', True, (coverImageId, contentItemId))
+=======
+            })
+
+
+    
+>>>>>>> Stashed changes
 
     while fetched_all == False:
         # Fetch people records from Rock.
@@ -107,7 +122,24 @@ def fetch_and_save_cover_image(ds, *args, **kwargs):
                 headers=headers)
                 
         contentItems = list(map(map_content_items, r.json()))
-        updatedItems = list(map(update_content_item_cover_image, contentItems))
+
+        #Sets all content items without a cover image to their parent's cover image
+        pg_hook.run(
+            """
+                UPDATE "contentItems"
+                SET    "coverImageId" = r."coverImageId"
+                FROM   (SELECT "contentItems".id AS childId,
+                            parent."coverImageId"
+                        FROM   "contentItems"
+                            INNER JOIN "contentItemsConnections"
+                                    ON "contentItemsConnections"."childId" =
+                                        "contentItems".id
+                            INNER JOIN "contentItems" AS parent
+                                    ON "contentItemsConnections"."parentId" = parent.id
+                        WHERE  "contentItems"."coverImageId" IS NULL) AS r
+                WHERE  r.childId = "contentItems".id;
+            """
+        )
 
         skip += top
         fetched_all = len(r.json()) < top
