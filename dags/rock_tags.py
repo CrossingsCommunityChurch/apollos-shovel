fa2f61607fea8f3b64291f89912296e8e89c3874
Vincent Wilson <vince@classyh.at>
2021-07-08 14:11:11 -0400

    Added persona tags dag

--- dags/rock_tags.py
+++ dags/rock_tags.py
@@ -98,3 +98,89 @@ def fetch_and_save_persona_tags(ds, *args, **kwargs):
         """
 
         pg_hook.run(add_apollos_ids)
+
+
+def attach_persona_tags_to_people(ds, *args, **kwargs):
+    if 'client' not in kwargs or kwargs['client'] is None:
+        raise Exception("You must configure a client for this operator")
+
+    headers = {"Authorization-Token": Variable.get(kwargs['client'] + "_rock_token")}
+
+    pg_connection = kwargs['client'] + '_apollos_postgres'
+    pg_hook = PostgresHook(
+        postgres_conn_id=pg_connection,
+        keepalives=1,
+        keepalives_idle=30,
+        keepalives_interval=10,
+        keepalives_count=5
+    )
+
+    person_entity_id = requests.get(
+        f"{Variable.get(kwargs['client'] + '_rock_api')}/EntityTypes",
+        params={"$filter": "Name eq 'Rock.Model.Person'"},
+        headers=headers
+    ).json()[0]['Id']
+
+    persona_params = {
+        "$filter": f"EntityTypeId eq {person_entity_id} and CategoryId eq {186}",
+        "$select": "Id,Name,Guid",
+        "$orderby": "ModifiedDateTime desc",
+    }
+
+    if not kwargs['do_backfill']:
+        persona_params['$filter'] += f" and (ModifiedDateTime ge datetime'{kwargs['execution_date'].strftime('%Y-%m-%dT00:00')}' or ModifiedDateTime eq null or PersistedLastRefreshDateTime ge datetime'{kwargs['execution_date'].strftime('%Y-%m-%dT00:00')})'"
+
+
+    personas = requests.get(
+            f"{Variable.get(kwargs['client'] + '_rock_api')}/DataViews",
+            params=persona_params,
+            headers=headers).json()
+
+
+    for persona in personas:
+        fetched_all = False
+        skip = 0
+        top = 10000
+
+        while not fetched_all:
+            params = {
+                "$top": top,
+                "$skip": skip,
+                "$select": "Id",
+                "$orderby": "ModifiedDateTime desc",
+            }
+
+            r = requests.get(
+                    f"{Variable.get(kwargs['client'] + '_rock_api')}/People/DataView/{persona['Id']}",
+                    params=params,
+                    headers=headers)
+            rock_objects = r.json()
+
+            if not isinstance(rock_objects, list):
+                print(rock_objects)
+                print("oh uh, we might have made a bad request")
+                print("top: {top}")
+                print("skip: {skip}")
+                skip += top
+                continue
+
+            def generate_insert_sql(tag_id, person_id):
+                return f'''
+                    INSERT INTO people_tag ("tagId", "personId", "createdAt", "updatedAt")
+                    SELECT t.id,
+                           p.id,
+                           NOW(),
+                           NOW()
+                    FROM people p,
+                         tags t
+                    WHERE t."originId" = '{tag_id}'
+                      AND p."originId" = '{person_id}'
+                    ON CONFLICT ("tagId", "personId") DO NOTHING
+                '''
+
+            sql_to_run = map(lambda p: generate_insert_sql(persona['Id'], p['Id']), rock_objects)
+
+            pg_hook.run(sql_to_run)
+
+            skip += top
+            fetched_all = len(rock_objects) < top            
