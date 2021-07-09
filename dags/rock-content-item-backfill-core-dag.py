872d46a2cb5f266694a464bcb9b96e31570cfdb2
Vincent Wilson <vince@classyh.at>
2021-07-09 14:09:31 -0400

    feat: Persona content dag (#23)
    * Added persona tag dag
    
    * Take a stab at the delta argument
    
    * Added persona tags dag
    
    * feat: Persona Content Dag

--- dags/rock-content-item-backfill-core-dag.py
+++ dags/rock-content-item-backfill-core-dag.py
@@ -7,7 +7,7 @@ from rock_media import fetch_and_save_media
 from rock_content_items_connections import fetch_and_save_content_items_connections, set_parent_id
 from rock_cover_image import fetch_and_save_cover_image
 from rock_content_item_categories import fetch_and_save_content_item_categories, attach_content_item_categories
-from rock_tags import fetch_and_save_persona_tags, attach_persona_tags_to_people
+from rock_tags import fetch_and_save_persona_tags, attach_persona_tags_to_people, attach_persona_tags_to_content
 from rock_features import fetch_and_save_features
 
 # Default settings applied to all tasks
@@ -73,6 +73,12 @@ with DAG('rock_content_item_backfill_core_dag',
     )
 
 
+    content_tags = PythonOperator(
+        task_id='attach_persona_tags_to_content',
+        python_callable=attach_persona_tags_to_content,
+        op_kwargs={'client': 'core', 'do_backfill': True}
+    )
+
     persona_tags = PythonOperator(
         task_id='attach_persona_tags_to_people',
         python_callable=attach_persona_tags_to_people,
@@ -82,6 +88,7 @@ with DAG('rock_content_item_backfill_core_dag',
     tags = PythonOperator(
         task_id='fetch_and_save_persona_tags',
         python_callable=fetch_and_save_persona_tags,  # make sure you don't include the () of the function
+        op_kwargs={'client': 'core', 'do_backfill': True}
     )
 
     features = PythonOperator(
@@ -97,7 +104,6 @@ with DAG('rock_content_item_backfill_core_dag',
 
     connections >> set_parent_id >> features
 
-    tags >> persona_tags
+    tags >> [persona_tags, content_tags]
 
     base_items >> [connections, media]
-
