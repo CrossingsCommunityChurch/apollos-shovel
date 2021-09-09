e9d8ade3effd0d5c558b3cf86282ee5dca5ee4d7
nlewis84 <nathan@differential.com>
2021-09-09 10:57:31 -0500

    camelcase tables

--- dags/vineyard-rock-people-dag.py
+++ dags/vineyard-rock-people-dag.py
@@ -9,13 +9,13 @@ from rock.rock_people_dag import create_rock_people_dag
 start_date = datetime(2021, 9, 8)
 
 backfill_dag, backfill_name = create_rock_people_dag(
-    "vineyard", start_date, "@once", True, camelcased_tables=True
+    "vineyard", start_date, "@once", True, camelcased_tables=False
 )
 
 globals()[backfill_name] = backfill_dag
 
 dag, name = create_rock_people_dag(
-    "vineyard", start_date, timedelta(minutes=30), False, camelcased_tables=True
+    "vineyard", start_date, timedelta(minutes=30), False, camelcased_tables=False
 )
 
 globals()[name] = dag
