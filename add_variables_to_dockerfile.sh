df06c1f5f13cf36f7da6e39058876d56712719d2
Vincent Wilson <vince@classyh.at>
2021-09-21 09:15:49 -0400

    Mask sensitive values in github actions

--- add_variables_to_dockerfile.sh
+++ add_variables_to_dockerfile.sh
@@ -1,5 +1,8 @@
 openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in .env.shared.enc -out .env.shared -k $ENV_SECRET
 
+# Mask values in Github actions output
+./mask_values.sh
+
 # Prepends the needed `ENV` docker command
 sed -i -e 's/^./ENV &/' .env.shared
 
