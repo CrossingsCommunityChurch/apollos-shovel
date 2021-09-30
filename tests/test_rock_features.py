00aacf525f4cb429e92c2ddc5b7940177d1fb4ca
Vincent Wilson <vince@classyh.at>
2021-09-30 10:08:44 -0400

    feat: Setup feature dag to support matrix values for scripture (#75)
    * Setup feature dag to support matrix values
    
    * Remove uneeded cassette
    
    * Fix situations with bad json
    
    * Be specific with errors
    
    * Best error
    
    * Fix linter

--- tests/test_rock_features.py
+++ tests/test_rock_features.py
@@ -67,7 +67,9 @@ def test_run_fetch_and_save_features(monkeypatch):
 
     with vcr.use_cassette("tests/cassettes/features/content_items.yaml"):
         content_item.run_fetch_and_save_content_items()
-    with vcr.use_cassette("tests/cassettes/features/initial_features.yaml"):
+    with vcr.use_cassette(
+        "tests/cassettes/features/initial_features.yaml", record_mode="new_episodes"
+    ):
         feature.run_fetch_and_save_features()
 
     conn = db_connect()
@@ -96,7 +98,10 @@ def test_run_fetch_and_save_features(monkeypatch):
                 i += 1
 
             # Add feature in the middle
-            with vcr.use_cassette("tests/cassettes/features/added_feature.yaml"):
+            with vcr.use_cassette(
+                "tests/cassettes/features/added_feature.yaml",
+                record_mode="new_episodes",
+            ):
                 feature.run_fetch_and_save_features()
 
             curs.execute(
@@ -120,7 +125,10 @@ def test_run_fetch_and_save_features(monkeypatch):
                 i += 1
 
             # Delete feature
-            with vcr.use_cassette("tests/cassettes/features/delete_feature.yaml"):
+            with vcr.use_cassette(
+                "tests/cassettes/features/delete_feature.yaml",
+                record_mode="new_episodes",
+            ):
                 feature.run_fetch_and_save_features()
 
             curs.execute(
@@ -142,3 +150,78 @@ def test_run_fetch_and_save_features(monkeypatch):
                 i += 1
 
     conn.close()
+
+
+def test_matrix_value_scriptures(monkeypatch):
+    def mock_get(config, deserialize_json=True, default_var=None):
+        if default_var:
+            return default_var
+        if "_rock_api" in config:
+            return "https://rock.apollos.app/api"
+        if "_rock_token" in config:
+            return "ASZjZWdf3IqrbZX9sedtB4wb"
+
+    monkeypatch.setattr(
+        Variable,
+        "get",
+        mock_get,
+    )
+
+    feature = Feature(
+        {
+            "client": "test",
+            "execution_date": datetime.datetime(
+                2005, 7, 14, 12, 30, tzinfo=datetime.timezone.utc
+            ),
+            "do_backfill": True,
+        }
+    )
+
+    feature.attribute_matrix_id = 10
+
+    content = {
+        "Id": "123",
+        "parent_origin_id": None,
+        "node_id": "some-guid-value",
+        "AttributeValues": {
+            "Scriptures": {
+                "Value": "12321-123213-123123",
+                "ValueFormatted": '{\n    "Attributes": [{\n            \n            "Book": "Psalm",\n            \n            "Reference": "22"\n    },{\n            \n            "Book": "Psalm",\n            \n            "Reference": "23"\n    },{\n            \n            "Book": "Psalm",\n            \n            "Reference": "24"\n    },{\n            \n            "Book": "Acts",\n            \n            "Reference": "17:1-15"\n    }]\n}',
+            }
+        },
+        "Attributes": {"Scriptures": {"FieldTypeId": 10}},
+    }
+
+    result = feature.get_features(content)
+
+    expected = [
+        {
+            "type": "Scripture",
+            "data": {"reference": "Psalm 22"},
+            "parent_id": "some-guid-value",
+            "priority": 0,
+        },
+        {
+            "type": "Scripture",
+            "data": {"reference": "Psalm 23"},
+            "parent_id": "some-guid-value",
+            "priority": 1,
+        },
+        {
+            "type": "Scripture",
+            "data": {"reference": "Psalm 24"},
+            "parent_id": "some-guid-value",
+            "priority": 2,
+        },
+        {
+            "type": "Scripture",
+            "data": {"reference": "Acts 17:1-15"},
+            "parent_id": "some-guid-value",
+            "priority": 3,
+        },
+    ]
+
+    i = 0
+    for ftr in result["added_features"]:
+        assert ftr == expected[i]
+        i += 1
