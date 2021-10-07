1c04fe6992cf8d486514da322fbb59a774d20234
Vincent Wilson <vincent@differential.com>
2021-10-07 11:51:13 -0400

    Wrong dag name!

--- dags/apollos-demo-rock-people-dag.py
+++ dags/apollos-demo-rock-people-dag.py
@@ -9,13 +9,13 @@ from rock.rock_people_dag import create_rock_people_dag
 start_date = datetime(2021, 10, 7)
 
 backfill_dag, backfill_name = create_rock_people_dag(
-    "apollos-demo", start_date, "@once", True, camelcased_tables=False
+    "apollos_demo", start_date, "@once", True, camelcased_tables=False
 )
 
 globals()[backfill_name] = backfill_dag
 
 dag, name = create_rock_people_dag(
-    "apollos-demo", start_date, timedelta(minutes=30), False, camelcased_tables=False
+    "apollos_demo", start_date, timedelta(minutes=30), False, camelcased_tables=False
 )
 
 globals()[name] = dag
--- dags/apollos-demo-rock-prayer-request-dag.py
+++ dags/apollos-demo-rock-prayer-request-dag.py
@@ -8,12 +8,12 @@ from rock.rock_prayer_request_dag import create_prayer_request_dag
 
 start_date = datetime(2021, 10, 7)
 
-dag, name = create_prayer_request_dag("apollos-demo", start_date, "@once", True)
+dag, name = create_prayer_request_dag("apollos_demo", start_date, "@once", True)
 
 globals()[name] = dag
 
 dag, name = create_prayer_request_dag(
-    "apollos-demo", start_date, timedelta(minutes=30), False
+    "apollos_demo", start_date, timedelta(minutes=30), False
 )
 
 globals()[name] = dag
