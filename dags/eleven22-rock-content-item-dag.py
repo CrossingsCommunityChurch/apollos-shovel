f2292bab3d7bcf402bac4b3cf0c6c92185c913c7
Vincent Wilson <vince@classyh.at>
2021-11-24 12:25:59 -0500

    Setups up Vimeo DAG and uses it on eleven22 (#104)
    * Setups up Vimeo DAG and uses it on eleven22
    
    * Restore vimeo dag

--- dags/eleven22-rock-content-item-dag.py
+++ dags/eleven22-rock-content-item-dag.py
@@ -3,6 +3,7 @@ from airflow import DAG  # noqa: F401
 from datetime import datetime, timedelta
 from rock.rock_content_item_dag import create_rock_content_item_dag
 from algolia_multitenant.algolia_content_dag import create_algolia_dag
+from vimeo.rock_vimeo import create_rock_vimeo_dag
 
 start_date = datetime(2021, 11, 18)
 
@@ -15,11 +16,19 @@ globals()[backfill_name] = backfill_dag
 dag, dag_name = create_rock_content_item_dag(
     "eleven22", start_date, timedelta(minutes=30), False
 )
-
 globals()[dag_name] = dag
 
 algolia_dag_name, algolia_dag = create_algolia_dag(
     "eleven22", start_date, timedelta(hours=12)
 )
-
 globals()[algolia_dag_name] = algolia_dag
+
+vimeo_backfill_dag, vimeo_backfill_name = create_rock_vimeo_dag(
+    "eleven22", start_date, "@once", True
+)
+globals()[vimeo_backfill_name] = vimeo_backfill_dag
+
+vimeo_dag, vimeo_name = create_rock_vimeo_dag(
+    "eleven22", timedelta(minutes=30), "@once", False
+)
+globals()[vimeo_name] = vimeo_dag
